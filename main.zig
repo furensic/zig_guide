a56bf5648b5112fa835d8d12febec50b4e8a1e24
Leo <info@furensic.eu>
2025-08-05 23:54:58 +0200

    Assignment in zig

--- main.zig
+++ main.zig
@@ -2,4 +2,12 @@ const std = @import("std");
 
 pub fn main() void {
     std.debug.print("Hello, {s}!\n", .{"World!"});
+
+    //-- Assignment
+    const contstant: i32 = 5; // signed 32-bit constant
+    const variable: u32 = 5000; // unsigned 32-bit variable
+
+    // @as performs an explicit type coercion
+    const inferred_constant = @as(i32, 5);
+    const inferred_variable = @as(u32, 5000);
 }

7fbac2f1c920af8314ee9a1a9cb42f8f77dd9444
Leo <info@furensic.eu>
2025-08-05 23:55:09 +0200

    Arrays in zig

--- main.zig
+++ main.zig
@@ -10,4 +10,9 @@ pub fn main() void {
     // @as performs an explicit type coercion
     const inferred_constant = @as(i32, 5);
     const inferred_variable = @as(u32, 5000);
+
+    //-- Arrays
+    const a = [5]u8{ 'h', 'e', 'l', 'l', 'o' }; // Array of type 8-bit unsigned integer with size of 5
+    const b = [_]u8{ 'w', 'o', 'r', 'l', 'd' }; // Array of type 8-bit unsigned interger with inferred size
+    const length = a.len; // Length of array a (5)
 }

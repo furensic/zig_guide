7501f80d3989278c3398a81d7400adacb15f3498
Leo <info@furensic.eu>
2025-08-06 21:23:23 +0200

    Trying out if with multiple conditions

--- test_if.zig
+++ test_if.zig
@@ -18,3 +18,13 @@ test "if statement expression" {
     x += if (a) 1 else 2;
     try expect(x == 1);
 }
+
+test "test if with multiple conditions" {
+    const is_true: bool = true;
+    const number: i32 = 10;
+    if (number == 10 and is_true == true) { // interesting, && doesn't work
+        try expect(true);
+    } else {
+        try expect(false);
+    }
+}

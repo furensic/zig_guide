51c038b6c76fcb6fe88304ed2aaf36942204c1aa
Leo <info@furensic.eu>
2025-08-06 12:43:21 +0200

    discarding unused variables

--- main.zig
+++ main.zig
@@ -6,13 +6,17 @@ pub fn main() void {
     //-- Assignment
     const contstant: i32 = 5; // signed 32-bit constant
     const variable: u32 = 5000; // unsigned 32-bit variable
+    _ = contstant + variable; // _ = var discard the variable, this prevents compiler errors if a variable is unused
 
     // @as performs an explicit type coercion
     const inferred_constant = @as(i32, 5);
     const inferred_variable = @as(u32, 5000);
+    _ = inferred_constant + inferred_variable;
 
     //-- Arrays
     const a = [5]u8{ 'h', 'e', 'l', 'l', 'o' }; // Array of type 8-bit unsigned integer with size of 5
     const b = [_]u8{ 'w', 'o', 'r', 'l', 'd' }; // Array of type 8-bit unsigned interger with inferred size
     const length = a.len; // Length of array a (5)
+    _ = b;
+    _ = length;
 }

8262599e2d679e019f26a6a27fc504595c8ea7a0
leo <info@furensic.eu>
2025-08-09 00:24:39 +0200

    Many Item Pointer in Zig

--- test_pointers.zig
+++ test_pointers.zig
@@ -4,3 +4,22 @@ test "usize" {
     try expect(@sizeOf(usize) == @sizeOf(*u8)); // not sure where this could be used?
     try expect(@sizeOf(isize) == @sizeOf(*u8));
 }
+
+//- Many item pointers
+fn doubleAllManypointer(buffer: [*]u8, byte_count: usize) void {
+    var i: usize = 0;
+    while (i < byte_count) : (i += 1) buffer[i] *= 2;
+}
+
+test "many-item pointer" {
+    var buffer: [100]u8 = [_]u8{1} ** 100;
+    const buffer_ptr: *[100]u8 = &buffer;
+
+    const buffer_many_ptr: [*]u8 = buffer_ptr;
+    doubleAllManypointer(buffer_many_ptr, buffer.len);
+    for (buffer) |byte| try expect(byte == 2);
+
+    const first_elem_ptr: *u8 = &buffer_many_ptr[0];
+    const first_elem_ptr_2: *u8 = @ptrCast(buffer_many_ptr);
+    try expect(first_elem_ptr == first_elem_ptr_2);
+}

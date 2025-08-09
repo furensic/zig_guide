55e477e15d24e3c0503f3f0f4d91fff8ecd63a8e
Leo <info@furensic.eu>
2025-08-09 11:56:11 +0200

    Runtime safety in Zig

---
+++ test_runtime_safety.zig
@@ -0,0 +1,31 @@
+const expect = @import("std").testing.expect;
+
+test "out of bounds" {
+    @setRuntimeSafety(false);
+    const a = [3]u8{ 1, 2, 3 };
+    var index: u8 = 5;
+    const b = a[index];
+
+    _ = b;
+    index = index;
+}
+
+test "unreachable" {
+    const x: i32 = 1;
+    // const y: u32 = if(x==2) 5 else unreachable;
+    // _ = y;
+    _ = x;
+}
+
+fn asciiToUpper(x: u8) u8 {
+    return switch(x){
+        'a'...'z' => x + 'A' - 'a',
+        'A'...'Z' => x,
+        else => unreachable,
+    };
+}
+
+test "unreachable switch" {
+    try expect(asciiToUpper('a') == 'A');
+    try expect(asciiToUpper('A') == 'A');
+}

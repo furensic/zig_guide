406f6eea6dbb31c892bff2b5568766ada903cf10
Leo <info@furensic.eu>
2025-08-09 11:55:59 +0200

    fizzbuzz in zig

---
+++ fizz_buzz.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    var count: u8 = 1;
+    while(count <= 100): (count += 1){
+        if (count % 3 == 0 and count % 5 == 0){
+            try stdout.writeAll("Fizz Buzz\n");
+        } else if(count % 5 == 0) {
+            try stdout.writeAll("Buzz\n");
+        } else if(count % 3 == 0) {
+            try stdout.writeAll("Fizz\n");
+        } else {
+            try stdout.print("{}\n", .{count});
+        }
+        const div_3: u2 = @intFromBool(count % 3 == 0);
+        const div_5 = @intFromBool(count % 5 == 0);
+        
+        // count 15
+        // div_3 = 1
+        // div_5 = 1
+        // 1 * 2 + 1 = 3 = 0b11
+        switch(div_3 << 1 | div_5) {
+            0b10 => try stdout.writeAll("Fizz\n"),
+            0b11 => try stdout.writeAll("Fizz Buzz\n"),
+            0b01 => try stdout.writeAll("Buzz\n"),
+            0b00 => try stdout.print("{}\n", .{count}),
+        }
+    }
+}

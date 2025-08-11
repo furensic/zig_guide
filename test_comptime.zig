a6c3f6aff1a323678e399fad4b0621f21a4a4685
Leo <info@furensic.eu>
2025-08-11 18:53:15 +0200

    comptime values in Zig

---
+++ test_comptime.zig
@@ -0,0 +1,25 @@
+const expect = @import("std").testing.expect;
+
+fn fibonacci(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+test "comptime blocks" {
+    const x = comptime fibonacci(10);
+    const y = comptime blk: {
+        break :blk fibonacci(10);
+    };
+    try expect(y == 55);
+    try expect(x == 55);
+}
+
+test "comptime_int" {
+    const a = 12;
+    const b = a + 10;
+
+    const c: u4 = a;
+    const d: f32 = b;
+    try expect(c == 12);
+    try expect(d == 22);
+}
